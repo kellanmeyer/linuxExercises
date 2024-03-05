05403ca5465f57495f4e6e2d0851c4ab2f7886fb
Rain <jhu364@wisc.edu>
2024-03-05 11:58:43 -0600

    digits and school implementation

--- school.sh
+++ school.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | {
+    total=0
+    count=0
+    while read value; do
+        total=$((total + value))
+        count=$((count + 1))
+    done
+    sum=$(echo "$total")
+    avg=$(echo "$total / $count" | bc -l)
+    echo "Total: $sum"
+    echo "Average: $avg"
+}
