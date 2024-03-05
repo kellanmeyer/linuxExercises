60c29b0539df6a601e57c7ab6ff351470c8b27db
Kellan <kmmeyer25@wisc.edu>
2024-03-05 11:20:15 -0600

    finished five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+mkdir five
+for i in {1..5}
+do
+    mkdir five/dir$i
+    for j in {1..4}
+    do
+	touch five/dir$i/file$j
+        for ((k=1; k<=$j; k++))
+	do
+	    echo $j >> five/dir$i/file$j
+	done
+    done
+done
